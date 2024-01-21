4dc8f76f44f31552bd0605ab3a1f2eff7f69be92
Marvin Deng <mdengomg2@gmail.com>
2024-01-20 19:42:05 -0800

    Add starter process code and update makefile with build instructions

--- Lab_1/pipe.c
+++ Lab_1/pipe.c
@@ -6,6 +6,22 @@
 
 int main(int argc, char *argv[])
 {
-	// TODO: it's all yours
+	pid_t child_pid;
+
+	child_pid = fork();
+
+	if (child_pid == -1) {
+		perror("fork");
+	}
+
+	// If fork returns 0, we are in the child
+	if(child_pid == 0) {
+		printf("In Child Process: Parent PID = %d, Child process PID = %d", getppid(), getpid());
+	} 
+	// Otherwise, we are in the parent
+	else {
+		printf("In Parent Process: Parent PID = %d, Child process PID = %d", getpid(), child_pid);
+	}
+
 	return 0;
 }
