e2553780ac8c86d78985e815462af8de9551e2d2
Marvin <mdengomg2@gmail.com>
2024-03-11 22:49:11 -0700

    feat: Add functions for pdf parsing and vector embedding

--- Cheat-Bot/app.py
+++ Cheat-Bot/app.py
@@ -11,3 +11,24 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 
+load_dotenv()
+
+genai.configure(api_key=os.getenv("GEMINI_KEY"))
+
+def get_pdf_text(pdf_docs) -> str:
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader=PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text: str) -> list:
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
